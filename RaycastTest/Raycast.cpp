d745febe0a6157e63744b1c9fd8afc4befd78494
realNikki <nikecoes@gmail.com>
2025-10-18 02:20:59 +0200

    sdl rect render test

--- RaycastTest/Raycast.cpp
+++ RaycastTest/Raycast.cpp
@@ -1,10 +1,9 @@
 #include <SDL3/SDL.h>
 
-int main(int argc, char* argv[]) {
+constexpr int WINDOW_WIDTH = 640;
+constexpr int WINDOW_HEIGHT = 480;
 
-    SDL_Window* window;
-    SDL_Renderer* renderer;
-
+int main(int /*argc*/, char* /*argv*/[]) {
     bool done = false;
 
     // Initialize SDL3
@@ -14,16 +13,31 @@ int main(int argc, char* argv[]) {
     }
     
     // Create window and renderer
-    window = SDL_CreateWindow("Raycast", 640, 480, 0);
-    renderer = SDL_CreateRenderer(window, nullptr);
+    SDL_Window* window = SDL_CreateWindow("Raycast", WINDOW_WIDTH, WINDOW_HEIGHT, 0);
+    SDL_Renderer* renderer = SDL_CreateRenderer( window, nullptr);
 
-    // Check that the window was successfully created
-    if (window == NULL) {
+    // Check that the window and renderer were successfully created
+    if (window == nullptr || renderer == nullptr) {
         // In the case that the window could not be made...
-        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window or renderer: %s\n", SDL_GetError());
         return 1;
     }
 
+    // Background color
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+    
+    SDL_FRect test_rect;
+    test_rect.w = 50;
+    test_rect.h = 50;
+    test_rect.x = 50;
+    test_rect.y = 50;
+
+    // Render rect in red
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderFillRect(renderer, &test_rect);
+    SDL_RenderPresent(renderer);
+    
     while (!done) {
         SDL_Event event;
 
